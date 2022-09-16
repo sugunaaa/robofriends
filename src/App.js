639ceed29a11f2276aa892905cb2644905f8639c
sugunaaa <sugunaalt@gmail.com>
2022-09-16 14:44:23 +0530

    search bar added with scrolling component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React,{Component} from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
-import Searchbox from "./Searchbox.js"
-
+import Searchbox from "./Searchbox.js";
+import Scroll from './Scroll';
+import './App.css'
 class App extends Component{
     constructor(){
         super()
         this.state={
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=> {this.setState({robots:users})});
+    }
+       
     onSearchChange=(event)=>{
         this.setState({searchfield:event.target.value })         
     }
@@ -19,13 +26,21 @@ class App extends Component{
             const filteredRobots=this.state.robots.filter(robots=>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());   
              })
+             if(this.state.robots.length===0)
+             {
+                return <h1>Loading...</h1>
+             }
+             else{
             return(
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <Searchbox SearchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
-        );  
-    }
+        ); 
+        }
+    }   
 }
 export default App;
