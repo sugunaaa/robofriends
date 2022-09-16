ba93393965d0a6d529db9266e8f0200bdb399560
sugunaaa <sugunaalt@gmail.com>
2022-09-16 11:07:08 +0530

    card-search

---
+++ src/Searchbox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Searchbox=({searchfield, SearchChange})=>{
+    return(
+        <div>
+           < input type='search'
+            className='pa3 ba b--green bg-lightest-blue' 
+            placeholder="search robots" 
+            onChange={SearchChange}/>
+        </div>
+    );
+}
+
+export default Searchbox;
