6b3e4a7f8d58499466e023a88a62fd9be9891b88
sugunaaa <sugunaalt@gmail.com>
2022-09-19 16:54:14 +0530

    Updates

---
+++ precache-manifest.f306e39d7855022058bceba6e6d9c99e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fb94e6cccc93be009d2",
+    "url": "/robofriends/static/css/main.32150b39.chunk.css"
+  },
+  {
+    "revision": "7fb94e6cccc93be009d2",
+    "url": "/robofriends/static/js/main.de0bb97d.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "19337e6523552aab9df3",
+    "url": "/robofriends/static/css/2.9019c6f2.chunk.css"
+  },
+  {
+    "revision": "19337e6523552aab9df3",
+    "url": "/robofriends/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6da33b7d1bd96c222dcf801376b7cc29",
+    "url": "/robofriends/index.html"
+  }
+];
