789463369972b1e607fe8fbda09852264b4020c5
sugunaaa <sugunaalt@gmail.com>
2022-09-20 15:26:26 +0530

    hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,24 @@
-import React,{Component} from "react";
+import React,{useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import Searchbox from "../components/Searchbox.js";
 import Scroll from '../components/Scroll';
 import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css'
-class App extends Component{
-    constructor(){
-        super()
-        this.state={
-            robots: [],
-            searchfield: ''
-        }
-    }
-
-    componentDidMount(){
+function App(){
+    const [robots, setRobots]=useState([]);
+    const [searchfield,setSearchField]=useState('');
+    const [count,setCount]=useState(0)
+    
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=>response.json())
-        .then(users=> {this.setState({robots:users})});
+        .then(users=> {setRobots    (users)});
+        console.log(count)
+    },[count])
+
+    const onSearchChange=(event)=>{
+        setSearchField(event.target.value)         
     }
-       
-    onSearchChange=(event)=>{
-        this.setState({searchfield:event.target.value })         
-    }
-    render(){
-            const{robots,searchfield}=this.state;
             const filteredRobots=robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());   
              })
@@ -32,7 +27,8 @@ class App extends Component{
             (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <Searchbox SearchChange={this.onSearchChange}/>
+                <button onClick={()=>setCount(count+1)}>Click Me!</button>
+                <Searchbox SearchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                          <CardList robots={filteredRobots}/>
@@ -41,5 +37,4 @@ class App extends Component{
             </div>
         ); 
         }
-    }   
 export default App;
